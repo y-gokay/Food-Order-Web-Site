274eb1bc220065136f2d50a4d891247d44708013
Yasin Gokay <ygokaykokdemir@outlook.com>
2024-12-05 10:27:22 +0300

    Update ScrollToUp.jsx

--- src/components/scripts/ScrollToUp.jsx
+++ src/components/scripts/ScrollToUp.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const ScrollToUp = ({ children }) => {
+const ScrollToUp = ({}) => {
   const { pathname } = useLocation();
-
+  // Sayfa geçişlerinde sayfayı yukarıya kaydırmayı sağlar
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-
-  return children || null;
 };
 
 export default ScrollToUp;
